6dbc095964f40f2321c8927f092156617619032d
Retired57 <149366058+Retired57@users.noreply.github.com>
2024-08-15 01:17:29 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Практическое задание по работе в PyCharm - "Переменные"
+
+amount_of_home_tasks = 12   # Количество выполненных ДЗ
+amount_of_hours = 1.5       # Количество затраченных часов
+course_name = "Python"      # Название курса
+time_for_task = 0.0         # Время на одно задание
+
+time_for_task = amount_of_hours / amount_of_home_tasks
+
+print("Курс: ", course_name, ", всего задач: ", amount_of_home_tasks, ", затрачено часов: ",amount_of_hours, ", среднее время выполнения: ", time_for_task, " часа.")
