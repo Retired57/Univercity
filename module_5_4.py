f54ae18d5546592869115ad96cd0d20aa5d3d185
Retired57 <149366058+Retired57@users.noreply.github.com>
2024-08-23 08:10:36 +0300

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -9,17 +9,14 @@ class House:
     def __new__(cls, *args, **kwargs):
         if cls.__instanse:
             cls.houses_history = []                 # создаем пустой список, но только 1 раз при создании 1-го объекта
-            cls.houses_history.append(args[0])      # каждый раз добавляем в список описание объекта
             cls.__instanse = False
-        else:
-            cls.houses_history.append(args[0])      # каждый раз добавляем в список описание объекта
         return super().__new__(cls)
 
 
     def __init__(self, name, number_of_floors):
         self.name = name                                # название ЖК, которому принадлежит дом
         self.number_of_floors = number_of_floors        # кол-во этажей в доме
-
+        self.houses_history.append(name)
 
     def __del__(self):
         print(f"{self.name} снесен, но он останется в истории.")
@@ -27,16 +24,16 @@ class House:
 
 # Т Е С Т Ы
 h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
+print(h1.houses_history)
 h2 = House('ЖК Акация', 20)
-print(House.houses_history)
+print(h2.houses_history)
 h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
+print(h3.houses_history)
 
 # Удаление объектов
 del h2
 del h3
-print(House.houses_history)
+print(h1.houses_history)
 print()
 print("Конец программы")        # по завершению проги деструктор ложно сообщит о сненсении последнего ЖК - "Эльбрус"
 print()
