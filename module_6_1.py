0a77b085854bfdb4fcb12176dc5bcfb3de484b0e
Retired57 <149366058+Retired57@users.noreply.github.com>
2024-08-26 13:35:04 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,85 @@
+# Домашнее задание по теме "Зачем нужно наследование"
+# Задача "Съедобное, несъедобное"
+
+# Ж И В О Т Н Ы Е
+class Animal:
+
+    def __init__(self, name, alive = True, fed = False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+
+    def eat(self, food):
+        if food.edible:
+            self.fed = True             # сытый
+            print(f"{self.name} съел {food.name}, живой и сытый")
+        else:
+            self.alive = False          # погиб
+            print(f"{self.name} съел {food.name}, погиб голодным")
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+# Р А С Т Е Н И Я
+class Plant:
+
+    def __init__(self, name, edible = False):
+        self.name = name
+        self.edible = edible
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+
+    def __init__(self, name, edible=False):
+        super().__init__(name)
+        super().__init__(edible)
+        self.edible = True
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Выполняемый код(для проверки):
+# ==============================
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+
+a1.eat(p1)
+a2.eat(p2)
+
+print(a1.alive)
+print(a2.fed)
+
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
+
+# Вывод на консоль:
+# =================
+#
+# Волк с Уолл-Стрит
+# Цветик семицветик
+# True
+# False
+# Волк с Уолл-Стрит не стал есть Цветик семицветик
+# Хатико съел Заводной апельсин
+# False
+# True
+
