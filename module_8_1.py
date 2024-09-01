77d1f1ad9c472455b0a75df03e079370cd0f0ef6
Retired57 <149366058+Retired57@users.noreply.github.com>
2024-09-01 21:24:22 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,32 @@
+# Домашнее задание по теме "Try и Except".
+# Задание "Программистам всё можно"
+
+
+def add_everything_up(a, b):
+    try:
+        a += b
+    except TypeError:
+        if (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, str)):  # a - число, b - строка
+            return str(a) + b
+        elif (isinstance(b, int) or isinstance(b, float)) and (isinstance(a, str)):  # а - строка, b - число
+            return a + str(b)
+
+    if isinstance(a, str):
+        return a                                        # сложили две строки
+    else:
+        return round(a, 3)                              # сложили два числа
+
+
+# Пример кода:
+#==============
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+print(add_everything_up('яблоко', 'строка'))
+
+# Вывод в консоль:
+#=================
+# 123.456строка
+# яблоко4215
+# 130.456
+# Здес
