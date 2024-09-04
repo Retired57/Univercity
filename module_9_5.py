d0963ad7dcf66f86350230fb8e48e0b2bbde2f28
Retired57 <149366058+Retired57@users.noreply.github.com>
2024-09-04 21:59:23 +0300

    Add files via upload

--- module_9_5.py
+++ module_9_5.py
@@ -7,7 +7,7 @@ class StepValueError(ValueError):
 
 class Iterator:
 
-    def __init__(self, start, stop, step = 1):
+    def __init__(self, start, stop, step=1):
         if step == 0:
             raise StepValueError('шаг не может быть равен 0!')
         else:
@@ -17,20 +17,21 @@ class Iterator:
             self.pointer = 0
 
     def __iter__(self):
-        self.pointer = self.start - self.step
+        self.pointer = self.start
         return self
 
     def __next__(self):
-        if ((self.step > 0 and (self.pointer + self.step) <= self.stop) or
-            (self.step < 0 and (self.pointer + self.step) >= self.stop)):
+        if ((self.step > 0 and (self.pointer <= self.stop)) or
+                (self.step < 0 and (self.pointer >= self.stop))):
+            self.ret = self.pointer
             self.pointer += self.step
         else:
             raise StopIteration()
-        return self.pointer
+        return self.ret
 
 
 # Пример выполняемого кода:
-#==========================
+# ==========================
 
 try:
     iter1 = Iterator(100, 200, 0)
@@ -43,7 +44,6 @@ iter2 = Iterator(-5, 1)
 iter3 = Iterator(6, 15, 2)
 iter4 = Iterator(5, 1, -1)
 iter5 = Iterator(10, 1, 1)
-
 
 for i in iter2:
     print(i, end=' ')
@@ -61,12 +61,10 @@ for i in iter5:
     print(i, end=' ')
 print()
 
-
 # Вывод на консоль:
-#==================
+# ==================
 
 # Шаг указан неверно
 # -5 -4 -3 -2 -1 0 1
 # 6 8 10 12 14
 # 5 4 3 2 1
-
